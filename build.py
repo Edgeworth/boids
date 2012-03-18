8a3ff59c799632c30bcf89a0102e853d1ff3c5cc
E <edgeworth.dev@gmail.com>
2012-03-18 15:48:34 +0800

    Added README and build script.

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import os, sys
+
+def error(val):
+	if val != 0:
+		print "Error during build."
+		sys.exit(1)
 
 flags = " -DCMAKE_BUILD_TYPE=Release "
 for arg in sys.argv[1:]:
 	if (arg == '-d'): flags = flags.replace('-DCMAKE_BUILD_TYPE=Release', '-DCMAKE_BUILD_TYPE=Debug')
 
 os.chdir('scripts')
-os.system('cmake .'+flags)
-os.system('make clean && make')
+error(os.system('cmake .'+flags))
+error(os.system('make clean && make'))
 os.chdir('../')
